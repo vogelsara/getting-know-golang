1c149641fb16e875521bddfa5d3989efcd68500d
Sari <sari.vogel.94@gmail.com>
2020-06-14 21:16:08 +0200

    hand-on exercise #5

--- hello.go
+++ hello.go
@@ -5,9 +5,15 @@ import "fmt"
 type book int
 
 var x book
+var y int
 
 func main () {
-	x = 42
 	fmt.Println(x)
 	fmt.Printf("%T,\n", x)
+
+	x = 10
+	fmt.Println(x)
+	y = int(x)
+	fmt.Println(y)
+	fmt.Printf("%T", y)
 }
