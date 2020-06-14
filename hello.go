8576826537761c92ffc397eb5461f7c3267539b0
Sari <sari.vogel.94@gmail.com>
2020-06-14 11:32:03 +0200

    hands-on exercise #2

--- hello.go
+++ hello.go
@@ -8,12 +8,8 @@ var z bool
 
 func main() {
 
-	x := 42
-	y := "James Bond"
-	z := true
-
-	fmt.Println(x)
-	fmt.Println(y)
-	fmt.Println(z)
+	fmt.Printf("%T\n", x)
+	fmt.Printf("%T\n", y)
+	fmt.Printf("%T\n", z)
 
 }
